439841f7e6234c361bf6a1831d9192a051467ab8
fmedinab <frankmedinas1997@gmail.com>
2025-05-14 18:24:07 -0500

    fix: update build configuration for root level HTML files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     outDir: 'dist',
-  },
-  base: '/',
+    rollupOptions: {
+      input: {
+        index: '/index.html',
+        categorias: '/categorias.html',
+        servicios: '/servicios.html',
+        tienda: '/tienda.html',
+        nosotros: '/nosotros.html',
+        contacto: '/contacto.html'
+      }
+    }
+  }
 })
